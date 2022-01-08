1c43c87fe9b05b8434fded749599e0d0dd0025ec
Bartosz Mazurkiewicz <bartek.mazurkiewicz69@gmail.com>
2022-01-08 23:22:03 +0100

    Added text and binary file copier

--- file-copier.cpp
+++ file-copier.cpp
@@ -1,20 +1,76 @@
-// file-copier.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
+bool fileExists(const char*);
+bool copyFile(const char*, const char*, char*, const char*);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(int argc, char** argv) {
+	if (argc < 4) {
+		printf_s("Brak odpowiednich parametrow.");
+	}
+	printf_s("Plik zrodlowy: %s\nPlik docelowy: %s\nTryb: %s\nRozmiar buffera: %s\n", argv[1], argv[2], argv[3], argv[4]);
+	
+	if (!fileExists(argv[1])) {
+		printf_s("Plik zrodlowy nie istnieje.\n");
+		return -1;
+	} 
+	if (fileExists(argv[2])) {
+		printf_s("Plik docelowy juz istnieje.\n");
+		return -1;
+	}
+
+	int bufferSize = atoi(argv[4]);
+	char* buffer = (char*)malloc(bufferSize);
+
+	if (copyFile(argv[1], argv[2], buffer, argv[3])) {
+		printf_s("Plik zostal skopiowany.\n");
+	}
+	
+	free(buffer);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool fileExists(const char* fileName) {
+	FILE* file;
+	if (file = fopen(fileName, "r")) {
+		fclose(file);
+		return true;
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	return false;
+}
+
+bool copyFile(const char* sourceFileName, const char* destFileName, char* buffer, const char* mode) {
+	FILE* sourceFile;
+	FILE* destFile;
+
+	if (strcmp(mode, "t") == 0) {
+		sourceFile = fopen(sourceFileName, "r");
+		destFile = fopen(destFileName, "w");
+	} else if (strcmp(mode, "b") == 0) {
+		sourceFile = fopen(sourceFileName, "rb");
+		destFile = fopen(destFileName, "wb");
+	}
+	else {
+		printf_s("Zly tryb:\n\tWybierz t dla trybu tekstowego lub b dla trybu binarnego.\n");
+		return false;
+	}
+
+	if (sourceFile == NULL || destFile == NULL) {
+		printf_s("Blad otwarcia pliku zrodlowego lub docelowego.\n");
+		return false;
+	}
+
+	size_t n;
+
+	while (!feof(sourceFile)) {
+		n = fread(buffer, 1, sizeof buffer, sourceFile);
+		fwrite(buffer, 1, n, destFile);
+	}
+
+	fclose(sourceFile);
+	fclose(destFile);
+
+	return true;
+}
+
